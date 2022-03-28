974e1a5ab72ad0fc92eb461005dd1d1d49cb6eb1
Park YeongSeo <Frog-Slayer@github.com>
2022-03-28 19:52:52 +0900

    회원 리포지토리 테스트 케이스 작성

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+
+    }
+
+
+}
