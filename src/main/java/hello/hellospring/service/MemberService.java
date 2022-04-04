8647fe8aa4a60598ff7c531c17360e860e6b918a
Park YeongSeo <Frog-Slayer@github.com>
2022-04-04 20:24:08 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,17 +1,42 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class MemberRepository {
-    private final MemberRepository memberRepository = new MemberRepository();
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member){
         //중복회원 금지
-        memberRepository.findByName(member.getName());
-
+        validateDuplicateMember(member); //중복회원 검증
         memberRepository.save(member);
-
-
+        return member.getId();
     }
 
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
 }
